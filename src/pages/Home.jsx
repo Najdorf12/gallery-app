332f890a5ef1d451b659bac5bbd066b517d1739b
Najdorf <agustin.morro@gmail.com>
2024-12-11 19:59:49 -0300

    add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,21 +37,26 @@ const cardsHomeData = [
 const Home = () => {
   return (
     <div className="w-full h-[100dvh] lg:h-screen z-50 relative flex flex-col justify-evenly items-center pt-2 gap-6">
-     
       <Navbar />
-     
+
       <article className="flex flex-col justify-center items-center relative z-50  w-full cursor-default">
         <h1 className="august-bold leading-none text-zinc-600 text-9xl  lg:text-[16rem] xl:text-[19rem] 2xl:text-[23rem] text-transparent bg-gradient-to-b bg-clip-text  from-grayCustom via-grayCustom  to-zinc-700">
           ART GALLERY
         </h1>
         <p className="text-sm  text-grayCustom font-text2 text-center text-pretty px-2 mt-4 lg:text-base lg:text-balance xl:max-w-[1000px] 2xl:max-w-[1300px] xl:text-xl  2xl:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis tempora est deserunt nulla doloremque ipsa molestias quasi magnam neque minima suscipit impedit animi labore ipsam, velit dolores accusamus iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis
+          tempora est deserunt nulla doloremque ipsa molestias quasi magnam
+          neque minima suscipit impedit animi labore ipsam, velit dolores
+          accusamus iste. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit.
         </p>
       </article>
 
       <section className="relative z-50  flex flex-wrap   justify-center items-center gap-x-9 gap-y-2 w-full   xl:gap-x-14 2xl:gap-x-20 ">
         {cardsHomeData.map((artist, i) => (
-          <CardsHome key={i} artist={artist} />
+          <a key={i} href="#productDetail">
+            <CardsHome  artist={artist} />
+          </a>
         ))}
       </section>
     </div>
