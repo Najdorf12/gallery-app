4f497420f1aa07fe63f2375dd852b12bbf4e3280
Najdorf <agustin.morro@gmail.com>
2024-12-11 19:55:16 -0300

    add styles

--- src/components/CardsHome.jsx
+++ src/components/CardsHome.jsx
@@ -1,8 +1,8 @@
 const CardsHome = ({ artist }) => {
   return (
-    <div className="relative z-50 border-l-[2px] border-b-[2px] border-zinc-600 w-[160px] rounded-sm h-[110px] pl-3 pt-1 lg:pl-4 lg:w-[220px] lg:h-[135px] xl:w-[240px] xl:h-[145px]  2xl:w-[250px] 2xl:h-[160px] hover:border-white duration-500 cursor-pointer">
+    <div className="relative z-50 border-l-[2px] border-b-[2px] border-zinc-600 w-[160px] rounded-sm h-[110px] pl-3 pt-1 lg:pl-3 lg:w-[220px] lg:h-[135px] xl:w-[240px] xl:h-[140px]  2xl:w-[250px] 2xl:h-[160px] hover:border-white duration-500 cursor-pointer">
       <figure>
-        <img src={artist?.icon} alt="icon" className="w-9 lg:w-12 xl:w-14 2xl:w-16" />
+        <img src={artist?.icon} alt="icon" className="w-9 lg:w-12 xl:w-12 2xl:w-16" />
       </figure>
       <article className="text-sm mt-4 font-text2 font-normal text-balance lg:text-base xl:text-lg lg:mt-6 2xl:text-lg">
         <h6 className="text-grayCustom">{artist?.name}</h6>
