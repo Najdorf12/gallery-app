9157c881976734f744310401bbc3ab02b6f74c28
Najdorf <agustin.morro@gmail.com>
2024-12-11 17:44:50 -0300

    add styles

--- src/components/TsParticlesBg.jsx
+++ src/components/TsParticlesBg.jsx
@@ -52,7 +52,7 @@ const TsParticlesBg = () => {
           speed: 1,
           straight: false,
         },
-        number: { density: { enable: true, area: 120 }, value: 90 },
+        number: { density: { enable: true, area: 120 }, value: 100 },
         opacity: {
           animation: { enable: false, minimumValue: 0.1, speed: 7, sync: false },
           random: false,
