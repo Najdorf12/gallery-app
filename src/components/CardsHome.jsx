2fb85344d84c750caa25a1e6a27dd3ead5548b76
Najdorf <agustin.morro@gmail.com>
2024-12-11 18:25:30 -0300

    add styles

--- src/components/CardsHome.jsx
+++ src/components/CardsHome.jsx
@@ -1,12 +1,12 @@
 const CardsHome = ({ artist }) => {
   return (
-    <div className="relative z-50 border-l border-b border-zinc-600 w-[160px] h-[110px] pl-3 lg:pl-6 lg:w-[210px] lg:h-[144px] 2xl:w-[250px] 2xl:h-[160px]">
+    <div className="relative z-50 border-l border-b border-zinc-600 w-[160px] rounded-sm h-[110px] pl-3 pt-1 lg:pl-4 lg:w-[240px] lg:h-[145px]  2xl:w-[250px] 2xl:h-[160px]">
       <figure>
-        <img src={artist?.icon} alt="icon" className="w-9 lg:w-12 xl:w-16 2xl:w-[72px]" />
+        <img src={artist?.icon} alt="icon" className="w-9 lg:w-12 xl:w-14 2xl:w-16" />
       </figure>
-      <article className="text-sm mt-4 font-text2 font-normal text-balance lg:text-base lg:mt-6 2xl:text-lg">
+      <article className="text-sm mt-4 font-text2 font-normal text-balance lg:text-base xl:text-lg lg:mt-6 2xl:text-lg">
         <h6 className="text-whiteCustom">{artist?.name}</h6>
-        <p  className="text-[12px] text-grayCustom lg:text-sm 2xl:text-base">{artist?.content}</p>
+        <p  className="text-[12px] text-grayCustom lg:text-base 2xl:text-base">{artist?.content}</p>
       </article>
     </div>
   );
