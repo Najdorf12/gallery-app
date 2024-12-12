a4741e1c227ddc7cc5f1a28b8f9165d7e8936da1
Najdorf <agustin.morro@gmail.com>
2024-12-12 00:14:43 -0300

    add styles

--- src/components/CardsHome.jsx
+++ src/components/CardsHome.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const CardsHome = ({ artist }) => {
   return (
     <Link to={`/${artist?.id}`}>
-      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-700 w-[180px] rounded-sm h-[114px] pl-3 pt-2  lg:pl-3 lg:w-[220px] lg:h-[140px] xl:w-[237px] xl:h-[145px]  2xl:w-[280px] 2xl:h-[167px] hover:border-white duration-500 cursor-pointer">
+      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-700 w-[180px] rounded-sm h-[114px] pl-3 pt-2  lg:pl-3 lg:w-[220px] lg:h-[133px] xl:w-[255px] xl:h-[152px]  2xl:w-[280px] 2xl:h-[178px] hover:border-white duration-500 cursor-pointer">
         <figure>
           <img
             src={artist?.icon}
@@ -11,8 +11,9 @@ const CardsHome = ({ artist }) => {
             className="w-10 h-10 object-cover object-center rounded-full lg:w-12 lg:h-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16"
           />
         </figure>
-        <article className="text-sm mt-4  md:text-balance lg:text-base xl:mt-5 xl:text-lg 2xl:mt-6  2xl:text-lg">
-          <h6 className="text-stone-300 font-text font-medium text-lg  xl:text-xl 2xl:text-2xl">
+        <article className="text-sm mt-4  md:text-balance lg:text-base xl:mt-5 xl:text-lg 2xl:mt-7  2xl:text-lg">
+          <div className="w-full h-[1px] bg-stone-700"></div>
+          <h6 className="text-stone-300 font-text font-medium text-base mt-1 lg:mt-2  xl:text-xl 2xl:text-2xl">
             {artist?.name}
           </h6>
           <p className="text-[13px] text-stone-500 font-text2 leading-3 lg:text-base lg:leading-4 2xl:text-lg ">
