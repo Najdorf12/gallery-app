140d8be3a9df0bcfe2c32348512f04cd28af054b
Najdorf <agustin.morro@gmail.com>
2024-12-12 19:54:42 -0300

    add styles

--- src/components/GalleryArtist.jsx
+++ src/components/GalleryArtist.jsx
@@ -1,4 +1,25 @@
+
+const obrasData = [
+  {
+    obraName: "History",
+    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    obraName: "History",
+    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    obraName: "History",
+    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    obraName: "History",
+    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+];
+
 const GalleryArtist = ({ images }) => {
+  
   return (
     <section className="flex flex-col lg:flex-row bg-red-500">
       <figure className="w-full px-2 lg:w-1/2 bg-teal-500 flex justify-center items-center">
@@ -8,22 +29,14 @@ const GalleryArtist = ({ images }) => {
           className="object-cover object-center w-full h-full max-w-[500px]"
         />
       </figure>
-      <div >
-        <div className="flex flex-wrap justify-center items-center gap-2 ">
-          <div className="border border-stone-500 h-[200px] w-[45%]">
-            Anatomy
-          </div>
-          <div className="border border-stone-500 h-[200px] w-[45%]">
-            History
-          </div>
-        </div>
-        <div className="flex flex-wrap justify-center items-center gap-2 ">
-          <div className="border border-stone-500 h-[200px] w-[45%]">
-            Astronomy
-          </div>
-          <div className="border border-stone-500 h-[200px] w-[45%]">
-            Philoshophy
-          </div>
+      <div>
+        <div className="flex flex-wrap justify-center items-center gap-2 px-2 ">
+          {obrasData?.map((obra, i) => (
+            <div key={i} className="border-l h-[200px] w-[46%]">
+              <p>{obra?.obraName}</p>
+              <p>{obra?.content}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
