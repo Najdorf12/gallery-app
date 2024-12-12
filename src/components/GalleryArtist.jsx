ad359169f1f76d36acdb0baefdd564ad9f86a114
Najdorf <agustin.morro@gmail.com>
2024-12-12 19:56:08 -0300

    add styles

--- src/components/GalleryArtist.jsx
+++ src/components/GalleryArtist.jsx
@@ -22,14 +22,14 @@ const GalleryArtist = ({ images }) => {
   
   return (
     <section className="flex flex-col lg:flex-row bg-red-500">
-      <figure className="w-full px-2 lg:w-1/2 bg-teal-500 flex justify-center items-center">
+      <figure className="w-full px-2  bg-teal-500 flex justify-center items-center lg:w-1/2">
         <img
           src={images[0]}
           alt="img-gallery"
           className="object-cover object-center w-full h-full max-w-[500px]"
         />
       </figure>
-      <div>
+      <div className="w-full lg:w-1/2 flex justify-center items-center">
         <div className="flex flex-wrap justify-center items-center gap-2 px-2 ">
           {obrasData?.map((obra, i) => (
             <div key={i} className="border-l h-[200px] w-[46%]">
