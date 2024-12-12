e7ee9f1ecc60ab59c0e8ab1a6322995364e8e256
Najdorf <agustin.morro@gmail.com>
2024-12-12 00:28:09 -0300

    add styles

--- src/components/CardsHome.jsx
+++ src/components/CardsHome.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 const CardsHome = ({ artist }) => {
   return (
     <Link to={`/${artist?.id}`}>
-      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-700 w-[180px] rounded-sm h-[114px] pl-3 pt-2  lg:pl-3 lg:w-[220px] lg:h-[133px] xl:w-[255px] xl:h-[152px]  2xl:w-[280px] 2xl:h-[178px] hover:border-white duration-500 cursor-pointer">
+      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-700 w-[180px] rounded-sm h-[114px] pl-3 pt-2  lg:pl-3 lg:w-[220px] lg:h-[133px] xl:w-[255px] xl:h-[152px]  2xl:w-[280px] 2xl:h-[178px] hover:border-stone-300 duration-700 cursor-pointer group">
         <figure>
           <img
             src={artist?.icon}
             alt="icon"
-            className="w-10 h-10 object-cover object-center rounded-full lg:w-12 lg:h-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16"
+            className="w-10 h-10 object-cover object-center rounded-full lg:w-12 lg:h-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16 group-hover:scale-110  duration-700"
           />
         </figure>
         <article className="text-sm mt-4  md:text-balance lg:text-base xl:mt-5 xl:text-lg 2xl:mt-7  2xl:text-lg">
-          <div className="w-full h-[1px] bg-stone-700"></div>
+          <div className="w-full h-[1px] bg-stone-700 "></div>
           <h6 className="text-stone-300 font-text font-medium text-base mt-1 lg:mt-2  xl:text-xl 2xl:text-2xl">
             {artist?.name}
           </h6>
