675dc770ccf0cd466b25eb43494840c0561d20b4
Najdorf <agustin.morro@gmail.com>
2024-12-14 12:42:19 -0300

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="absolute top-0 z-[600] w-full flex justify-center items-center pt-2 lg:pt-2 2xl:pt-6 ">
+      <Link to={"/"}>
+        <div className="absolute z-[650] left-3 top-1">
+          <i className="bx bxl-graphql text-2xl lg:text-4xl text-redCustom"></i>
+        </div>
+      </Link>
       <ul className="w-full flex flex-row  justify-center items-center gap-5 font-text font-medium text-grayCustom text-sm  lg:gap-12 lg:text-lg xl:gap-16">
         <li className="hover:text-whiteCustom duration-700 cursor-pointer hover:scale-110">
           Plano
