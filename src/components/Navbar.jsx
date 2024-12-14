57f6a3ee94ebacd5486559e9ff993b4986af3273
Najdorf <agustin.morro@gmail.com>
2024-12-14 12:43:21 -0300

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <nav className="absolute top-0 z-[600] w-full flex justify-center items-center pt-2 lg:pt-2 2xl:pt-6 ">
       <Link to={"/"}>
-        <div className="absolute z-[650] left-3 top-1">
-          <i className="bx bxl-graphql text-2xl lg:text-4xl text-redCustom"></i>
+        <div className="absolute z-[650] left-3 top-1 flex justify-center items-center">
+          <i className="bx bxl-graphql text-2xl lg:text-4xl text-grayCustom"></i>
         </div>
       </Link>
       <ul className="w-full flex flex-row  justify-center items-center gap-5 font-text font-medium text-grayCustom text-sm  lg:gap-12 lg:text-lg xl:gap-16">
