009cb40ee9e4e1d1d3dd864324a7af462c1215db
Najdorf <agustin.morro@gmail.com>
2024-12-14 17:29:15 -0300

    add styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,11 +32,11 @@ const About = ({ handleButtonClick }) => {
   return (
     <section className="flex flex-col  h-screen justify-evenly lg:flex-row lg:items-center">
       <article className="flex flex-col justify-start items-start relative z-50 w-full cursor-default lg:max-w-[60%] lg:self-start lg:mt-28">
-        <h1 className="august-bold leading-none text-zinc-300 text-8xl lg:text-[8rem] 2xl:text-[10rem]">
+        <h1 className="august-bold leading-none text-zinc-300 text-8xl lg:text-[8rem] xl:text-[10rem] 2xl:text-[12rem]">
           ABOUT
         </h1>
         <div className="w-12 h-[1px] bg-grayCustom mt-3  lg:w-[300px] lg:mt-6"></div>
-        <p className="text-sm text-grayCustom font-text2 font-medium text-balance mt-4 lg:text-base lg:mt-6 max-w-[660px] xl:text-lg 2xl:text-xl">
+        <p className="text-sm text-grayCustom font-text2 font-medium text-balance mt-4 lg:text-base lg:mt-6 max-w-[660px] xl:text-lg 2xl:text-xl 2xl:max-w-[700px]">
           Discover our unique approach to building robust digital solutions. We
           provide cutting-edge strategies to empower your brand online.
         </p>
@@ -60,7 +60,7 @@ const About = ({ handleButtonClick }) => {
       <ul className="flex flex-col gap-1 relative z-50 w-full mt-6 lg:self-end lg:pb-3">
         {artistsBtns?.map((artist, i) => (
           <li key={i} className="flex  border-b border-grayCustom py-2 max-w-[500px] lg:max-w-[700px]">
-            <p className="august-bold text-zinc-300 text-5xl w-[70%] lg:text-6xl xl:text-7xl">{artist?.name.toLocaleUpperCase()}</p>
+            <p className="august-bold text-zinc-300 text-5xl w-[70%] lg:text-6xl xl:text-7xl 2xl:text-8xl">{artist?.name.toLocaleUpperCase()}</p>
             <div className="flex flex-col justify-between text-grayCustom text-sm font-text xl:text-base">
               {artist?.data1}
               <div>{artist?.data2}</div>
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,19 +1,45 @@
 import TsParticlesBg from "../components/TsParticlesBg";
 import Navbar from "../components/Navbar";
 
+const registroData = [
+  {
+    name: "design",
+    data: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quia magnam recusandae, quod dolor quam similique voluptatibus",
+  },
+  {
+    name: "development",
+    data: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quia magnam recusandae, quod dolor quam similique voluptatibus",
+  },
+  {
+    name: "marketing",
+    data: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quia magnam recusandae, quod dolor quam similique voluptatibus",
+  },
+];
 const Registro = () => {
   return (
-    <section className="relative  w-full h-[100dvh] lg:h-screen z-40 flex flex-col  bg-blackCustom overflow-hidden">
-       <TsParticlesBg /> 
+    <section className="relative  w-full min-h-screen z-40 flex flex-col  bg-blackCustom overflow-hidden">
+      <TsParticlesBg />
       <Navbar />
       <div className="w-full h-[100dvh]  z-[550] relative flex flex-col mt-20 pt-4 ">
-        <article className="flex flex-col justify-center items-center relative z-50 px-3  w-full cursor-default">
-          <h1 className="august-bold text-balance leading-[4rem] text-zinc-300 text-6xl ">
-            Lorem ipsum dolor sit amet <span className="text-redCustom">consectetur adipisicing</span> elit. Nisi
-            facilis tempora est deserunt
-          </h1>
-         
+        <article className="flex flex-col justify-center items-center relative z-50 px-3 w-full cursor-default max-w-[800px]">
+          <p className="august-bold text-balance leading-[4rem] text-zinc-300 text-6xl ">
+            Lorem ipsum dolor sit amet{" "}
+            <span className="text-redCustom">consectetur adipisicing</span>{" "}
+            elit. Nisi facilis tempora est deserunt
+          </p>
         </article>
+        <ul className=" py-12 flex flex-col justify-center items-center gap-2">
+          {registroData?.map((data, i) => (
+            <li key={i} className=" relative flex py-2 max-w-[650px]">
+              <p className="text-whiteCustom w-96 pl-2 border">{data?.name}</p>
+              <div className="text-grayCustom text-balance">{data?.data}</div>
+              <div className="absolute z-50 -bottom-3 right-1 flex gap-3 justify-center items-center md:relative md:self-end text-grayCustom">
+                <div className="w-32 h-[2px] bg-whiteCustom"></div>
+                {data?.name}
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
