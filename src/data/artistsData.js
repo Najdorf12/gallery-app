1cd9920ea1e9f8b77358540939a1a7c1774cbfc3
Najdorf <agustin.morro@gmail.com>
2024-12-16 01:03:25 -0300

    add styles

--- src/data/artistsData.js
+++ src/data/artistsData.js
@@ -11,21 +11,21 @@ import imgLau4 from "/artists/LauraAguirre/04.jpg";
 import imgLau5 from "/artists/LauraAguirre/05.jpg";
 
 import imgEdu1 from "/artists/EduHinojosa/01.jpg";
-import imgEdu2 from "/artists/EduHinojosa/02.jpg";
+import imgEdu2 from "/artists/EduHinojosa/04.jpg";
 import imgEdu3 from "/artists/EduHinojosa/03.jpg";
-import imgEdu4 from "/artists/EduHinojosa/04.jpg";
+import imgEdu4 from "/artists/EduHinojosa/02.jpg";
 import imgEdu5 from "/artists/EduHinojosa/01.jpg";
 
 import imgVaras1 from "/artists/VarasMackenzi/01.jpg";
 import imgVaras2 from "/artists/VarasMackenzi/02.jpg";
 import imgVaras3 from "/artists/VarasMackenzi/03.jpg";
-import imgVaras4 from "/artists/VarasMackenzi/04.jpg";
-import imgVaras5 from "/artists/VarasMackenzi/01.jpg";
+import imgVaras5 from "/artists/VarasMackenzi/04.jpg";
+import imgVaras4 from "/artists/VarasMackenzi/01.jpg";
 
 import imgAlvarex1 from "/artists/Alvarex/01.jpg";
 import imgAlvarex2 from "/artists/Alvarex/02.jpg";
-import imgAlvarex3 from "/artists/Alvarex/03.jpg";
-import imgAlvarex4 from "/artists/Alvarex/04.jpg";
+import imgAlvarex4 from "/artists/Alvarex/03.jpg";
+import imgAlvarex3 from "/artists/Alvarex/04.jpg";
 import imgAlvarex5 from "/artists/Alvarex/05.jpg";
 
 import imgBisy1 from "/artists/Alvarex/01.jpg";
