0cbc18ec1eab98b4819d90a895fa45f356b47bde
Najdorf <agustin.morro@gmail.com>
2024-12-15 22:19:38 -0300

    add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,19 @@ const Home = ({ artistData }) => {
   return (
     <main className="relative w-full h-[100dvh] lg:h-screen z-50 flex flex-col justify-center items-center overflow-hidden">
       <TsParticlesBg />
-      <div className="w-full h-[100dvh] lg:h-screen z-[550] relative flex flex-col justify-evenly items-center pt-4 ">
+      <div className="w-full h-[100dvh] lg:h-screen z-[550] relative flex flex-col justify-evenly items-center pt-6 ">
         <Navbar />
 
         <article className="flex flex-col justify-center items-center relative z-50  w-full cursor-default">
-          <h1 className="august-bold leading-none bg-gradient-to-b bg-clip-text text-transparent from-grayCustom via-grayCustom to-zinc-300 text-9xl lg:text-[16rem] xl:text-[18.2rem] 2xl:text-[22rem]">
-            ART GALLERY
+          <h1 className="august-bold leading-none bg-gradient-to-b bg-clip-text text-transparent from-grayCustom via-grayCustom to-zinc-300 text-[5.6rem] px-2 text-center lg:text-[16rem] xl:text-[18.2rem] 2xl:text-[22rem]">
+            GALERÍA INVISIBLE
           </h1>
-          <p className="text-sm  text-grayCustom font-text2 font-medium text-center text-pretty px-2 mt-4 lg:text-base lg:text-balance xl:max-w-[1000px] 2xl:max-w-[1300px] xl:text-lg  2xl:text-xl">
+          <div className="text-stone-500 font-text2 mt-1 flex justify-center items-center gap-4 text-base lg:mt-3 lg:text-xl">
+            <span className="w-20 h-[1px] bg-stone-600 lg:w-[400px] xl:w-[500px]"></span>
+            EXPOSICION COLECTIVA{" "}
+            <span className="w-20 h-[1px] bg-stone-600 lg:w-[400px] xl:w-[500px]"></span>
+          </div>
+          <p className="text-sm  text-grayCustom font-text2 font-medium text-center text-pretty px-2 mt-2 lg:text-base lg:text-balance xl:max-w-[1500px] 2xl:max-w-[1500px] xl:text-lg  2xl:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
             facilis tempora est deserunt nulla doloremque ipsa molestias quasi
             magnam neque minima suscipit impedit animi labore ipsam, velit
@@ -23,7 +28,7 @@ const Home = ({ artistData }) => {
           </p>
         </article>
 
-        <section className="relative z-50  flex flex-wrap mt-3 justify-center items-center gap-x-4 gap-y-5 w-full lg:self-end  lg:gap-x-6 xl:gap-x-9 2xl:gap-x-14 ">
+        <section className="relative z-50  flex flex-wrap  justify-center items-center gap-x-4 gap-y-5 w-full lg:self-end  lg:gap-x-6 xl:gap-x-9 2xl:gap-x-14 ">
           {artistsData?.map((artist, i) => (
             <CardsHome key={i} artist={artist} />
           ))}
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -24,13 +24,13 @@ const Registro = () => {
       <Navbar />
       <div className="w-full h-[100dvh]  z-[550] relative flex flex-col mt-14  items-center lg:flex-row lg:mt-0">
         <article className="flex flex-col justify-center items-center relative z-50 px-1 w-full cursor-default max-w-[800px] lg:self-start lg:w-[50%]  lg:mt-24">
-          <p className="august-bold text-center text-balance leading-[4rem] text-zinc-300 text-6xl lg:text-7xl lg:leading-[5rem]  2xl:text-8xl 2xl:leading-[6.5rem]">
+          <p className="august-bold text-center text-balance leading-[3.4rem] px-1 text-zinc-300 text-5xl lg:text-7xl lg:leading-[5rem]  2xl:text-8xl 2xl:leading-[6.5rem]">
             Lorem ipsum dolor sit amet{" "}
             <span className="text-redCustom">consectetur adipisicing</span>{" "}
             elit. Nisi facilis tempora est deserunt
           </p>
         </article>
-        <ul className="pt-14 flex flex-col justify-center items-center gap-3 px-4 lg:w-[50%]">
+        <ul className="pt-16 flex flex-col justify-center items-center gap-3 px-4 lg:w-[50%]">
           {registroData?.map((data, i) => (
             <li key={i} className=" relative flex flex-col py-6 max-w-[650px] font-text2 xl:max-w-[600px]">
               <p className="text-whiteCustom  pl-2 border py-[2px]">{data?.name}</p>
