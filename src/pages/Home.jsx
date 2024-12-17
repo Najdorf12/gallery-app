3d83194ee71659c7219c873d5b3e0cc103e0b34f
Najdorf <agustin.morro@gmail.com>
2024-12-16 22:38:37 -0300

    add animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,53 +5,66 @@ import { artistsData } from "../data/artistsData";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
-const Home = ({ artistData }) => {
+const Home = () => {
   const titleRef = useRef(null);
   const lineRef = useRef(null);
   const lineRef2 = useRef(null);
   const descriptionRef = useRef(null);
+  const cardsRef = useRef([]); // <-- Array de referencias para las cards
 
   useEffect(() => {
     const timeline = gsap.timeline({
       defaults: { duration: 1, ease: "power1" },
     });
 
-    // Animar las líneas simultáneamente
+    // Animaciones iniciales
     timeline.fromTo(
       lineRef.current,
       { scaleX: 0, opacity: 0 },
-      { scaleX: 1, opacity: 1, transformOrigin: "center", duration: 1 }
+      { scaleX: 1, opacity: 1, transformOrigin: "center" }
     );
+
     timeline.fromTo(
       lineRef2.current,
       { scaleX: 0, opacity: 0 },
-      { scaleX: 1, opacity: 1, transformOrigin: "center", duration: 1 },
+      { scaleX: 1, opacity: 1, transformOrigin: "center" },
       "<"
     );
 
-    // Animar el título
     timeline.fromTo(
       titleRef.current,
       { y: 30, opacity: 0 },
       { y: 0, opacity: 1, delay: 0.4 },
-      "-=0.5" // Ajusta para solapar con la animación previa
+      "-=0.5"
     );
 
-    // Animar el párrafo de descripción
     timeline.fromTo(
       descriptionRef.current,
       { y: 15, opacity: 0 },
       { y: 0, opacity: 1 },
-      "-=0.01" // Ajusta para solapar con la animación previa
+      "-=0.01"
+    );
+
+    // Animación de las cards una por una
+    gsap.fromTo(
+      cardsRef.current,
+      { y: 30, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        stagger: 0.4, // <-- Retraso entre cada card
+        ease: "power1",
+      }
     );
   }, []);
 
   return (
     <main className="relative w-full h-[100dvh] lg:h-screen z-50 flex flex-col justify-center items-center overflow-hidden">
       <TsParticlesBg />
-      <div className="w-full h-[100dvh] lg:h-screen z-[550] relative flex flex-col justify-evenly gap-5 items-center pt-7 ">
+      <div className="w-full h-[100dvh] lg:h-screen z-[550] relative flex flex-col justify-evenly gap-5 items-center pt-7">
         <Navbar />
-        <article className="flex flex-col justify-center items-center relative z-50  w-full cursor-default">
+        <article className="flex flex-col justify-center items-center relative z-50 w-full cursor-default">
           <h1
             ref={titleRef}
             className="august-bold leading-none bg-gradient-to-b bg-clip-text text-transparent from-grayCustom via-grayCustom to-zinc-300 text-[5.6rem] px-2 text-center lg:text-[16rem] xl:text-[18.2rem] 2xl:text-[22rem]"
@@ -71,21 +84,27 @@ const Home = ({ artistData }) => {
           </div>
           <p
             ref={descriptionRef}
-            className="text-sm  text-grayCustom font-text2 font-medium text-center text-pretty px-2 mt-2 lg:text-base lg:text-balance xl:max-w-[1500px] 2xl:max-w-[1500px] xl:text-lg  2xl:text-xl"
+            className="text-sm text-grayCustom font-text2 font-medium text-center text-pretty px-2 mt-2 lg:text-base lg:text-balance xl:max-w-[1500px] 2xl:max-w-[1500px] xl:text-lg 2xl:text-xl"
           >
             Galería Invisible nace como el catálogo de la exposición colectiva
             “Artistas Gráficos de Valpo por el Mundo”, proyecto que reúne a seis
             artistas contemporáneos de Valparaíso. Este grupo de artistas,
-            utilizan la gráfica y el soporte bidimensional como herramienta
+            utilizan la gráfica y el soporte bidimensional, como herramienta
             discursiva para desarrollar una investigación estética, poética y
             narrativa, que representa su imaginario visual
             porteño-latinoamericanista.
           </p>
         </article>
 
-        <section className="relative z-50  flex flex-wrap  justify-center items-center gap-x-5 gap-y-5 w-full lg:self-end  lg:gap-x-6 xl:gap-x-9 2xl:gap-x-14 ">
+        <section className="relative z-50 flex flex-wrap justify-center items-center gap-x-5 gap-y-5 w-full lg:gap-x-6 xl:gap-x-9 2xl:gap-x-14">
           {artistsData?.map((artist, i) => (
-            <CardsHome key={i} artist={artist} />
+            <div
+              key={i}
+              ref={(el) => (cardsRef.current[i] = el)} // Asigna la referencia
+              id="card"
+            >
+              <CardsHome artist={artist} />
+            </div>
           ))}
         </section>
       </div>
