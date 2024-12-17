5292856c9a2da70aefab969d98aef2c2b60746d0
Najdorf <agustin.morro@gmail.com>
2024-12-16 23:18:35 -0300

    add animations

--- src/pages/Plano.jsx
+++ src/pages/Plano.jsx
@@ -1,61 +1,66 @@
 import TsParticlesBg from "../components/TsParticlesBg";
 import Navbar from "../components/Navbar";
-import arrow from "/arrow.png";
-import imgPlano1 from "/planos/00.jpg"
-import imgPlano2 from "/planos/01.jpg"
-import imgPlano3 from "/planos/02.jpg"
+import arrow from "/arrow4.png";
+import imgPlano1 from "/planos/00.jpg";
+import imgPlano2 from "/planos/01.jpg";
+import imgPlano3 from "/planos/02.jpg";
 
 const planosBtns = [
   {
     name: "01 - LOREM IMPSUM",
     data1: "Lorem impsum dolor sit amet concsectur. Some random text",
     data2: "Lorem impsum",
-    img: imgPlano1
+    img: imgPlano1,
   },
   {
     name: "02 - LOREM IMSUM",
     data1: "Lorem impsum dolor sit amet concsectur. Some random text",
     data2: "Lorem impsum",
-    img: imgPlano2
+    img: imgPlano2,
   },
   {
     name: "03 - LOREM IMSUM ",
     data1: "Lorem impsum dolor sit amet concsectur. Some random text",
     data2: "Lorem impsum",
-    img: imgPlano3
+    img: imgPlano3,
   },
-
 ];
 
 const Plano = () => {
   return (
-    <section className="flex flex-col bg-blackCustom lg:items-center  overflow-hidden pb-12 lg:h-screen lg:pb-0 lg:justify-center" >
+    <section className="flex flex-col bg-blackCustom lg:items-center  overflow-hidden pb-12 lg:h-screen lg:pb-0 lg:justify-center">
       <TsParticlesBg />
       <Navbar />
-      <article className="flex flex-col px-3 justify-start items-start relative z-50 w-full cursor-default mt-14 lg:max-w-[60%] lg:self-start lg:mt-28 lg:pl-[5%] lg:flex-row">
-        <h1 className="august-bold leading-none text-zinc-300  text-8xl lg:text-[8rem] xl:text-[10rem] 2xl:tsext-[12rem]">
-          PLANO
-        </h1>
-        <div className="w-12 h-[1px] bg-grayCustom mt-3  lg:w-[300px] lg:mt-6"></div>
-        <p className="text-sm text-grayCustom font-text2 font-medium text-balance mt-4 lg:text-base lg:mt-6 max-w-[660px] xl:text-lg 2xl:text-xl 2xl:max-w-[700px]">
+      <article className="flex flex-col px-3 justify-start items-start relative z-50 w-full cursor-default mt-14  lg:items-center lg:justify-between lg:mt-16  lg:flex-row">
+        <div className="lg:pl-[10%]">
+          <h1 className="august-bold leading-none text-zinc-300  text-8xl  lg:text-[8rem] xl:text-[9.5rem] 2xl:text-[12rem]">
+            PLANO
+          </h1>
+          <div className="w-12 h-[1px] bg-grayCustom mt-3  lg:w-[300px] lg:mt-6"></div>
+        </div>
+        <p className="text-sm relative text-grayCustom font-text2 font-medium text-balance mt-4 lg:text-base lg:mt-6 max-w-[660px] xl:text-lg 2xl:text-xl 2xl:max-w-[700px]">
           Discover our unique approach to building robust digital solutions. We
-          provide cutting-edge strategies to empower your brand online.
-          Discover our unique approach to building robust digital solutions. We
-          provide cutting-edge strategies to empower your brand online.
+          provide cutting-edge strategies to empower your brand online. Discover
+          our unique approach to building robust digital solutions. We provide
+          cutting-edge strategies to empower your brand online.
+        
         </p>
-       
       </article>
-      <ul className="flex flex-col gap-3 relative z-50 w-full mt-12 px-5  lg:flex-row ">
+      <ul className="flex flex-col gap-6 relative z-50 w-full mt-12 px-2  lg:flex-row lg:px-4">
         {planosBtns?.map((plano, i) => (
           <li
             key={i}
-            className="flex flex-col  border-b border-grayCustom py-3 max-w-[500px]  xl:max-w-[600px] rounded-br-md border-r pl-3"
+            className="flex flex-col border-b pb-4 max-w-[500px] border-stone-600   rounded-tr-lg rounded-br-md border-r "
           >
-            <img src={plano?.img} alt="" />
-            <p className="august-bold text-zinc-300 text-5xl w-[70%] lg:text-6xl  2xl:text-7xl">
+            <img
+              className="w-full h-full object-cover rounded-t-lg "
+              src={plano?.img}
+              alt="plano-img"
+            />
+            <p className="august-bold text-zinc-300 mt-5 pl-2 text-5xl w-[70%] lg:text-6xl  2xl:text-7xl">
               {plano?.name.toLocaleUpperCase()}
             </p>
-            <div className=" text-grayCustom text-balance mt-3 text-sm font-text xl:text-base">
+            <div className=" text-grayCustom text-balance mt-3 pl-2 text-sm font-text xl:text-base">
               {plano?.data1}
             </div>
           </li>
